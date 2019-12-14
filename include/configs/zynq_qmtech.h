1a2fb64ef75536890f3afe7ef1bf07f324644509
Martin Chabot <martin.chabot@gmail.com>
2019-12-14 19:42:22 +0100

    Add QMTech Zynq starter kit support
    Signed-off-by: Martin Chabot <martin.chabot@gmail.com>
    Signed-off-by: Julien Olivain <juju@cotds.org>

---
+++ include/configs/zynq_qmtech.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2013 Xilinx, Inc.
+ * (C) 2019 Julien Olivain <juju@cotds.org>
+ *
+ * Configuration settings for the QMTECH Zynq boards
+ * See zynq-common.h for Zynq common configs
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_ZYNQ_QMTECH_H
+#define __CONFIG_ZYNQ_QMTECH_H
+
+#define CONFIG_SYS_MMC_ENV_DEV 0
+
+#include <configs/zynq-common.h>
+
+#endif /* __CONFIG_ZYNQ_QMTECH_H */
